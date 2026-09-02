71e1d942be30fcda2035e0faa81c75a1d67852d3
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-02 09:24:15 +0500

    better code

--- PY-P2.py
+++ PY-P2.py
@@ -1,7 +1,18 @@
 print("Largest numbers")
 
-def find_largest(numbers):
-    return max(numbers)
-numbers = 1, 4 ,6, 9, 634
-answer = find_largest(numbers)
-print(answer)
+number = int
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+c = int(input("Enter value for c: "))
+d = int(input("Enter value for d: "))
+e = int(input("Enter value for e: "))
+
+def find_largest(number):
+    largest = number[0]
+    for numbers in number:
+     if numbers > largest:
+        largest = numbers
+    return largest
+number = [a,b,c,d,e]
+result = find_largest(number)
+print(f"The largest number is {result}")
