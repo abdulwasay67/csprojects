62c93dd622e8e467e3280e960f3fd9a89f54f621
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-05 09:15:48 +0500

    .

---
+++ PY-P8.py
@@ -0,0 +1,8 @@
+person = {
+    "name": input("ENTER YOUR NAME: "),
+    "age": int(input("ENTER UR AGE IN NUMBERS: ")),
+    "course": input("ENTER YOUR COURSE: ")
+}
+print(person["name"])
+print("\n", person["age"])
+print("\n", person["course"])
