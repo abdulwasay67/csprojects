101ad18b3897081f5c2b1454edb695d335286f62
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-09 07:11:12 +0500

    const and vectors

---
+++ R9-C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    const int max_tries = 3;
+    cout << "THE MAX TRIES ARE:  "<< max_tries << endl;
+    max_tries = 55
+}
+int main(){
+    vector<int> nums;
+    nums.push_back(10);
+    nums.push_back(20);
+    nums.push_back(30);
+     
+    for(int x : nums){
+        cout << x <<" " << endl;
+    }
+
+}
