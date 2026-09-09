f275da6f9b1fe771a6fc50b32a329b38b7bd7a29
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-09 06:07:35 +0500

    switch

--- R7-C.cpp
+++ R7-C.cpp
@@ -6,23 +6,23 @@ int main() {
     cout << "CHOOSE: 1) START 2) SETTINGS 3) SAVE 4) EXIT" << endl;
     cin >> choice;
     switch (choice){
-        case '1';{
+        case 1:{
             cout << "GAME STARTING......" << endl;
             break;
         }
-        case '2';{
+        case 2:{
             cout << "SETTINGS INTERFACE" << endl;
             break;
         }
-        case '3';{
+        case 3:{
             cout << "TOTAL SAVES AVALIBLE" << endl;
             break;
         }
-        case '4';{
+        case 4:{
             cout << "EXITING......" << endl;
             break;
         }
-        default;{
+        default:{
             cout << "INVALID OPERATION!" << endl;
         }
 
