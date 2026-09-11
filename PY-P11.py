c5807f0769141c2ca4ae87474dc45310eecdf168
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-11 11:52:26 +0500

    del keys

--- PY-P11.py
+++ PY-P11.py
@@ -11,3 +11,17 @@ try:
     print(key)
 except KeyError:
     print("NOT A KEY!")
+
+id = {
+    "name": "john doe",
+    "age": 19,
+    "height": 5.4,
+    "blood type": 'A'
+}
+print(id.keys())
+print(id.items())
+print(id.values())
+execute = list(id.keys())
+print(execute)
+for id, value in id.items():
+    print(id, value)
