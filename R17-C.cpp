2a6cf3728bd0c8d563940957f9187f182fd9e61d
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-22 13:22:24 +0500

    encapsulation

--- R17-C.cpp
+++ R17-C.cpp
@@ -2,15 +2,37 @@
 using namespace std;
 
 class Car{
-    public:
-    string brand;
-    string model;
-    int speed;
+private:
+   string brand;
+   string model;
+   int speed;
+    
+public: 
+     void setNAME(string brand){
+        (if name == "Toyota" || name == "Honda")
+        NAME = brand;
+     }
+     string getNAME(){
+        return NAME;
+     }    
+     void setMODEL(string model){
+        MODEL = model;
+     }
+     string getMODEL(){
+        return MODEL;
+     }
+     void setSPEED(int speed){
+        if(speed >= 100)
+        SPEED = speed;
+     }
+     int getSPEED(){
+        return speed;
+     }
 
     void info(){
-    cout << brand << "" << endl;
-    cout << model << "" << endl;
-    cout << speed << "" << endl;
+    cout << "NAME - " << NAME << endl;
+    cout << "MODEL - " << MODEL << endl;
+    cout << "SPEED - " << SPEED << endl;
     }
     Car(string NAME, string MODEL, int SPEED){
         brand = NAME;
@@ -21,4 +43,14 @@ class Car{
 int main(){
     Car car1 = Car("Toyota", "Camry", 250);
     car1.info();
+
+    Car car2 = Car("Honda" , "Accord", 225);
+    car2.info();
+    
+    car1.getSPEED(50);
+    cout << car1.getNAME() << " max speed is " << car1.getSPEED() << " ! " << endl;
+
+    return 0;
+    
+
 }
