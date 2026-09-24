3b18e39f76b63d27df6b96ae515024c7131b221b
abdul wasay imran <abdulwasayimran4@gmail.com>
2026-09-24 10:54:28 +0500

    fixes

--- R18-C.cpp
+++ R18-C.cpp
@@ -1,60 +1,70 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class AbstractEmployee{ 
-    virtual void AskForPromotion()=0
+class AbstractEmployee {
+   virtual void AskForPromotion() = 0;
 };
-class Employee:AbstractEmployee{
- private:
-  string name;
-  string company;
-  int age;
- public: 
- void setNAME(string name){
-    name = NAME;
- }
- string getNAME(){
-    return NAME;
- }
- void setCOMPANY(string company){
-    company = COMPANY;
- }
- string getCOMPANY(){
-    return COMPANY;
- }
- void getAGE(int age){
-    if( age > 18){
-        age = AGE;
-    }
-    else{
-        cout << "YOU CANT BE UNDERAGED!" << endl;
-    }
- int getAGE(){
-    return AGE;
- }
- }
 
- void info(){
-    cout << "THE NAME IS " << NAME << endl;
-    cout << NAME << "IS " << AGE << "YEARS OLD." << endl;
-    cout << NAME << "WORKS FOR " << COMPANY << endl;
- }
-  void AskForPromotion(){
-    if (age > 30){
-        cout << "ELIGIBLE FOR PROMOTION!" << endl;
-    }
-    else{
-        cout << "NOT ELIGIBLE FOR PROMOTION" << endl;
-    }
- }
+class Employee : public AbstractEmployee {
+private:
+   string name;
+   string company;
+   int age;
 
+public:
+   Employee(string name, string company, int age)
+      : name(name), company(company), age(age) {}
+
+   void setNAME(string name) {
+      this->name = name;
+   }
+
+   string getNAME() {
+      return name;
+   }
+
+   void setCOMPANY(string company) {
+      this->company = company;
+   }
+
+   string getCOMPANY() {
+      return company;
+   }
+
+   void setAGE(int age) {
+      if (age > 18) {
+         this->age = age;
+      } else {
+         cout << "YOU CANT BE UNDERAGED!" << endl;
+      }
+   }
+
+   int getAGE() {
+      return age;
+   }
+
+   void info() {
+      cout << "THE NAME IS " << name << endl;
+      cout << name << " IS " << age << " YEARS OLD." << endl;
+      cout << name << " WORKS FOR " << company << endl;
+   }
+
+   void AskForPromotion() override {
+      if (age > 30) {
+         cout << "ELIGIBLE FOR PROMOTION!" << endl;
+      } else {
+         cout << "NOT ELIGIBLE FOR PROMOTION" << endl;
+      }
+   }
 };
-int main(){
-    Employee e1 = Employee("Arthur Morgan", "Saint Louis pvt. ltd", 49);
-    e1.info();
-    Employee e2 = Employee("Lucia Sahur", "Miami Capital", 27);
-    e2.info();
 
-    e1.AskForPromotion();
-    e2.AskForPromotion();
+int main() {
+   Employee e1("Arthur Morgan", "Saint Louis pvt. ltd", 49);
+   e1.info();
+   Employee e2("Lucia Sahur", "Miami Capital", 27);
+   e2.info();
+
+   e1.AskForPromotion();
+   e2.AskForPromotion();
 }
